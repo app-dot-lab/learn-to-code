dbe3fe1105df4d88487651730c8784e4c27e9e3c
Ashwin Cherian <iamashwincherian@gmail.com>
2021-06-17 09:32:58 +0530

    Add jwt as header in axios

--- client/src/api/backend.js
+++ client/src/api/backend.js
@@ -1,7 +1,21 @@
 import axios from 'axios'
+import { useSelector } from 'react-redux'
+
+// Find a way to implement redux
+
+const getToken = () => {
+    try {
+        return JSON.parse(localStorage.getItem('authUser')).token
+    } catch {
+        return ''
+    }
+}
 
 const Backend = axios.create({
-    baseURL: 'http://localhost:5000/'
+    baseURL: 'http://localhost:5000/',
+    headers: {
+        Authorization: "Bearer " + getToken()
+    }
 })
 
 export default Backend
