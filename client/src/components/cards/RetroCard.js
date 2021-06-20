ff7368c9bd436761cd1e16961227ffb3022cda2c
Ashwin Cherian <iamashwincherian@gmail.com>
2021-06-20 15:48:26 +0530

    Add retro card component

---
+++ client/src/components/cards/RetroCard.js
@@ -0,0 +1,18 @@
+import { Card } from 'react-bootstrap'
+
+const RetroCard = (props) => {
+    return (
+        <Card className='card-retro cursor-pointer mb-2'>
+            <Card.Body>
+                <div className="card-retro-content">
+                    { props.img && <Card.Img variant='top' src={props.img} /> }
+                    <div className="p-3">
+                        {props.children}
+                    </div>
+                </div>
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default RetroCard
