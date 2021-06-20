ea657fcd015c538b76887aeb4125605a526b6a73
Ashwin Cherian <iamashwincherian@gmail.com>
2021-06-20 15:46:16 +0530

    Add persistor

--- client/src/index.js
+++ client/src/index.js
@@ -2,20 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { applyMiddleware, createStore, compose } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import App from './components/App'
 import reducers from './reducers'
 import { AuthReducerMiddleware } from './middlewares/reducers/auth'
+import { ThemeReducerMiddleware } from './middlewares/reducers/themes'
 import { getDefaultReduxState } from './utils/auth'
 
 import 'normalize.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
+import './styles.scss';
 
+const persistedReducer = persistReducer({key: 'root', storage}, reducers)
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const enhancer = composeEnhancers(applyMiddleware(AuthReducerMiddleware))
-
-const store = createStore(reducers, getDefaultReduxState(), enhancer)
+const enhancer = composeEnhancers(applyMiddleware(ThemeReducerMiddleware))
+const store = createStore(persistedReducer, enhancer)
+const persistor = persistStore(store)
 
 ReactDOM.render(
     <Provider store={store}>
