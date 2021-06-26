9dfeaa8747a51ad81d75e5bd6d84f53d1be06462
Ashwin Cherian <iamashwincherian@gmail.com>
2021-06-26 06:19:46 +0530

    Set theme when the app redenders

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { DARK_MODE, LIGHT_MODE } from "../actions/types";
@@ -11,20 +11,21 @@ import Home from "./Home";
 import Posts from "./posts/Posts";
 import Login from "./Login";
 import Logout from "./Logout";
-import PostItem from "./PostItem";
-import NewPost from "./posts/NewPost";
+import PostItem from "../routes/posts/PostItem";
+import NewPost from "../routes/posts/NewPost";
+import EditPost from "../routes/posts/EditPost";
 import Search from "./Search";
 import IDE from "./ide/IDE";
 import Settings from "./settings/Settings";
 
 const App = () => {
     const [searchActive, setSearchActive] = useState(false)
-    const storeState = useSelector(state => state)
-    const auth = storeState.auth
+    const storeStates = useSelector(state => state)
+    const auth = storeStates.auth
 
     // Change this logic
 
-    const theme = storeState.theme
+    const theme = storeStates.theme
     if (theme.mode == DARK_MODE) {
         document.getElementsByTagName('body')[0].classList.add('dark')
     }
@@ -63,7 +64,7 @@ const App = () => {
                 <ProtectedRoute isAuthenticated={auth.isLoggedIn} path="/posts/new" exact component={NewPost} />
                 <ProtectedRoute isAuthenticated={auth.isLoggedIn} path="/ide" exact component={IDE} />
                 <Route path="/posts/:id" exact component={PostItem} />
-                {/* <Route path="/ide" exact component={IDE} /> */}
+                <Route path="/posts/:id/edit" exact component={EditPost} />
                 <Route path="/login" exact component={Login}/>
                 <Route path="/logout" exact component={Logout}/>
                 <Route path="/settings" exact component={Settings}/>
