3eb34cca795c53eece75faa34bdb812284f33d6c
Abdul Jaseem <jaseemabdul20@gmail.com>
2021-07-05 09:43:21 +0530

    Add Post routes and controller

--- server/controllers/posts_controller.js
+++ server/controllers/posts_controller.js
@@ -1,9 +1,14 @@
+
 class PostsController{
     constructor(opts){
         this.logger= opts.logger
     }
     async getPosts(req,res,next){
         this.logger.info('PostsController getPosts() called')
+    }
+    async getPost(req,res,next){
+        this.logger.info(`PostController getPost(${req.params.id}) called`)
+
     }
 }
 module.exports = PostsController;
--- server/routes/post_routes.js
+++ server/routes/post_routes.js
@@ -3,4 +3,7 @@ module.exports=(router,serviceLocator)=>{
     router.get('/posts',(req,res,next)=>{
         PostsController.getPosts(req,res,next)
     })
+    router.get('/posts/:id',(req,res,next)=>{
+        PostsController.getPost(req,res,next);
+    })
 }
