6c8aeee20df6fe8541a922c7b591132e78cc9192
Abdul Jaseem <jaseemabdul20@gmail.com>
2021-07-07 06:04:17 +0530

    Delete server code from repo

--- server/routes/auth_routes.js
+++
@@ -1,8 +0,0 @@
-module.exports=(router,serviceLocator)=>{
-    const userController = serviceLocator.get('userController')
-    const logger = serviceLocator.get('logger') 
-    const passport = serviceLocator.get('passport')
-    router.post('/login',passport.authenticate("local"),(req,res,next)=>{
-        userController.login(req,res,next);
-    })
-}
--- server/routes/post_routes.js
+++
@@ -1,9 +0,0 @@
-module.exports=(router,serviceLocator)=>{
-    const PostsController = serviceLocator.get('postsController'); 
-    router.get('/posts',(req,res,next)=>{
-        PostsController.getPosts(req,res,next)
-    })
-    router.get('/posts/:id',(req,res,next)=>{
-        PostsController.getPost(req,res,next);
-    })
-}
