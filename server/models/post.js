ad479062b4865078391bfbc5520fd781c4973d71
Ashwin Cherian <iamashwincherian@gmail.com>
2021-07-29 06:41:21 +0530

    Add published field in post schema

--- server/models/post.js
+++ server/models/post.js
@@ -15,6 +15,11 @@ const postSchema = new Schema({
         ref: "User",
         required: true
     },
+    published: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
 });
 
 module.exports = mongoose.model("Post", postSchema);
